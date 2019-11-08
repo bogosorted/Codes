617e15440bfb7a00cc6bdd0cc528493d9b270178
Aluno <gabrielaragaofds@gmail.com>
2019-11-08 18:52:10 -0300

    2.3

--- CardGame/card/Program.cs
+++ CardGame/card/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace card
 {
@@ -6,47 +7,70 @@ namespace card
     {
         class Deck
         {
-            List<Card> mao = new List<Card>();
+            float angulacaoMaxZ;
 
+            List<Card> mao = new List<Card>();
+            public Deck(float z)
+            {
+                this.angulacaoMaxZ = z;
+            }
+            
             public Card Carta
             {
                 get { return Carta; }
-                set { mao.Add(value);}
+                set { mao.Add(value); }
             }
             public int GetTamanhoBaralho()
             {
                 return mao.Count;
             }
-            public class Card : Deck
+            public float SetarAngulacao(List<Card> a)
             {
-                float id, angulacaoMaxZ;
-                public int Ataque {get; set;}
-                public int Vida {get; set;}
-                public string Nome {get; set;}
-                
-                public Card(float id, Deck a)
+             for (float i = 0; i < a.Count; i++)
+                 {
+                    a[i].Angulacao = a.Count % 2 == 0 ? angulacaoMaxZ / (float)(a.Count / 2) : angulacaoMaxZ / (float)((a.Count - 1) / 2);
+                 }              
+            }
+
+        }
+        class Card
+        {
+            float id;
+            public int Angulacao { get; set; }
+            public int Ataque { get; set; }
+            public int Vida { get; set; }
+            public string Nome { get; set; }
+
+            public Card(float id, Deck a)
+            {
+                a.Carta = this;
+                this.id = id;
+                switch (id)
                 {
-                    a.Carta = this;
-                    this.id = id;
-                    switch (id)
-                    {
-                        case 0:
-                            this.Nome = "Trovador";
-                            this.Ataque = 15;
-                            this.Vida = 13;
-                            break;
-                    }
+                    case 0:
+                        this.Nome = "Trovador";
+                        this.Ataque = 15;
+                        this.Vida = 13;
+                        break;
                 }
             }
-            static void Main(string[] args)
-            {
-                Deck primeiro = new Deck();
-                Card trova = new Card(0,primeiro);
-                Deck segundo = new Deck();
-                Card b = new Card(0, segundo);
-                Card o = new Card(0, segundo);
-               
-            }
         }
+        static void Main(string[] args)
+        {
+            Deck primeiro = new Deck(3);
+            Deck segundo = new Deck(3);
+            int zM = 3;
+            int quantidCart = 17;
+         /*   float angulação = quantidCart % 2 == 0f ? zM /(float)(quantidCart / 2) : zM / (float)((quantidCart - 1) / 2);
+            for (float i = -zM; i <= zM;)
+            {               
+                Console.WriteLine(i);
+                i += angulação;
+                if (quantidCart % 2 == 0 && i == 0)
+                    i += angulação;
+            }
+            */
+            
+        }       
     }
 }
