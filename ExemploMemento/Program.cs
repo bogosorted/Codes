cafd52af8c6594555d8f854d8b6cb8a7361d9bfd
José Roberto <gabrielaragaofds@gmail.com>
2019-11-26 15:01:31 -0300

    3.2

--- ExemploMemento/Program.cs
+++ ExemploMemento/Program.cs
@@ -43,8 +43,22 @@ namespace ConsoleApplication43
             }
             public Commits SalvarCommits()
             {
+                Console.WriteLine("Salvando versão...");
                 return new Commits(_dataModificacao, _nomeArquivo, _textoArquivo, _diretorio);
             }
+            public void VoltarCommits(Commits a)
+            {
+                Console.WriteLine("Regredindo versão...");
+                this._nomeArquivo = a.NomeArquivo;
+                this._diretorio = a.Diretorio;
+                this._textoArquivo = a.Texto;
+            }
+            public void ExibirInfo()
+            {
+                Console.WriteLine( "Data de modificação: {0}",_dataModificacao);
+                //Console.WriteLine( "Nome do arquivo")
+            }
+            
         }
         #endregion
         #region Memento
@@ -61,14 +75,30 @@ namespace ConsoleApplication43
                 this._nomeArquivo = b;
                 this._textoArquivo = c;
                 this._diretorio = d;
-            }           
+            }
+            public string NomeArquivo
+            {
+                get { return _nomeArquivo; }
+            }
+            public string Diretorio
+            {
+                get { return _diretorio; }
+            }
+            public string Texto
+            {
+                get { return _textoArquivo; }
+
+            }
+
         }
         #endregion
         #region CareTaker
         class Github
         {
-            private Stack<Commits> _mementos;
-            public Commits CareTaker { get{ return _mementos.Pop(); } set { _mementos.Push(value); } }
+            private Stack<Commits> _mementos = new Stack<Commits>();
+            public Commits CareTaker
+            { get{ return _mementos.Pop(); }
+              set {_mementos.Push(value); } }
         }
         #endregion
 
@@ -76,12 +106,19 @@ namespace ConsoleApplication43
         static void Main(string[] args)
         {
             File a = new File();
-            a.Diretorio = @"C:\Users\aluno\Music";
+            a.Diretorio = @"c";
             a.NomeArquivo = "Musicas";
             a.Texto = "Eu curto rock balboa";
           
             Github b = new Github();
             b.CareTaker = a.SalvarCommits();
+
+            a.Diretorio = @"C:\Users\aluno\Object3D";
+            a.NomeArquivo = "Maçã";
+            a.Texto = "Maçã verde, cultivada em 1987 e mumificada pelo própio Isaaac Newton";
+
+            b.CareTaker = a.SalvarCommits();
+
         }
     }
 }
